d5db0165c3b94772f03ba7dd0cc35bf85b25a25f
Logan <logan@terra.money>
2020-12-09 20:19:35 +0900

    news section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
 $(window).load(function () {
     $(".container").css( "opacity", "1" );
+    $(".media-carousel").owlCarousel({
+        loop: false,
+        nav: false,
+        URLhashListener: true,
+        startPosition: 'URLHash',
+        autoplayHoverPause: false,
+        responsive: {
+            0: {
+                items: 1,
+                margin: 10
+            },
+            480: {
+                items: 2,
+                margin: 15
+            },
+            1024: {
+                items: 2,
+                margin: 20
+            }
+        }
+    });
+
+    // if(window.location.hash) {
+    //     $([document.documentElement, document.body]).animate({
+    //         scrollTop: $("#news").offset().top
+    //     }, 1);
+    //   }
 });
